416111228106508d3ff12a7bf289659dbe9b3739
Gbrito-code <gabrielbritodesousa@gmail.com>
2022-03-25 11:40:45 -0300

    pagina de contato e footer simples

--- js/main.js
+++ js/main.js
@@ -87,17 +87,6 @@ $(function () {
 });
 
 
-/* carrossel parcerias*/
-const root = document.documentElement;
-const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
-const marqueeContent = document.querySelector("ul.marquee-content");
-
-root.style.setProperty("--marquee-elements", marqueeContent.children.length);
-
-for (let i = 0; i < marqueeElementsDisplayed; i++) {
-	marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
-}
-/* carrossel parcerias*/
 
 
 
@@ -139,7 +128,8 @@ const debounce = function(func, wait, immediate) {
 	window.addEventListener('scroll', debounce(function() {
 	  animeScroll();
 	}, 200));
-  }
+  };
+
 
 //Animação do scroll
 
